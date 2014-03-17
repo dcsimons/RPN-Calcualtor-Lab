b1d7f10d8dc9d2b6ae772f31107bc1b0e4d6b82a
David Simons <dave.c.simons@gmail.com>
2014-03-17 16:09:21 -0700

    functional stack.rb file

--- stack.rb
+++ stack.rb
@@ -2,4 +2,43 @@
 # Do not use any ruby arrays!!
 
 class Stack
+	attr_reader :length
+	attr_accessor :tail
+	def initialize ()
+		@length = 0
+		@tail = nil
+	end
+
+	def push(value)
+		new_node = Node.new(value)
+    @length += 1
+    if @tail.nil?
+      @tail = new_node
+    else
+      new_node.previous = @tail
+      @tail = new_node
+    end
+	end
+
+	 def pop
+    if @tail.nil?
+      return nil
+    else
+      popped_tail = @tail
+      @tail = @tail.previous
+      @length -= 1
+      return popped_tail.value
+    end
+  end
+end
+
+# A simple Node class.  A LinkList will be composed of many
+# nodes.
+class Node
+  attr_accessor :next, :previous, :value
+  def initialize(value)
+    @value = value
+    @next = nil
+    @previous = nil
+  end
 end
